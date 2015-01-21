be91ac4714f3a22f51fb425de4700fea81b1e704
awdebruijn <a.w.debruijn@gmail.com>
2015-01-21 17:51:36 +0100

    Content gestyled
    beeldmateriaal erin gestopt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,10 @@
  $(document).ready(function() {
- 	// custom jquery shit
- });
+ 	// navbar link fix, nodig ivm hoogte fixed navbar
+ 	var offset = 90;
+
+	$('.navbar li a').click(function(event) {
+	    event.preventDefault();
+	    $($(this).attr('href'))[0].scrollIntoView();
+	    scrollBy(0, -offset);
+	});
+});
